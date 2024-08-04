9e3a91a132fc3f0c0a276f03644f6f9062271dbf
Welton <welton.carvalho.dev@gmail.com>
2024-08-04 13:22:59 -0300

    initial commit

---
+++ src/main/java/com/arc/arc_register_clothes/ArcRegisterClothesApplication.java
@@ -0,0 +1,13 @@
+package com.arc.arc_register_clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcRegisterClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcRegisterClothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arc/arc_register_clothes/ArcRegisterClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arc.arc_register_clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcRegisterClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
